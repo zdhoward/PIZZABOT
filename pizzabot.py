e1fb2e3e03155ade0511f2a6448990ee5587a379
0x01 <zach.d.howard@gmail.com>
2019-03-25 16:06:36 -0700

    Adding Parameters To PIZZABOT

--- pizzabot.py
+++ pizzabot.py
@@ -1,100 +1,60 @@
-import discord
+from discord.ext import commands
 import asyncio
-import argparse
 from datetime import date
 
 from credentials import secret_key, client_id, token
 from cl_scraper import cl_search
 
-client = discord.Client()
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-k','--keys', '--keywords', nargs='+', help='Add keywords to a search', dest='keys', required=True)
-parser.add_argument('-s','--sort', help='Name of the sort filter', dest='sort', required=False)
-parser.add_argument('-v', '-d','--verbose', '--debug', action='store_true', dest='debug', required=False)
-parser.add_argument('-l', '--location', help='Filter for a particular location', dest='filterLocationData', required=False)
-parser.add_argument('-p', '--price', help='Filter for a particular price', dest='filterPriceData', required=False)
-parser.add_argument('-f', '--free', '--not-free', help='Filter out free things', action='store_true', dest='filterFree', required=False)
+### CONFIG ###
+prefix = "!"
+bot = commands.Bot(command_prefix='!')
 
 def Log(msg):
     print(str(date.today()) + ": " + msg)
 
-@client.event
-async def on_message(message):
-    global parser
-    #Do not reply to self
-    if message.author == client.user:
-        return
+def botMsg(msg):
+    return ("```" + str(msg) + "```")
 
-    ## HELLO COMMAND
-    if message.content.startswith("!hello") and message.channel.name == "craigslist":
-        Log("{0.author.mention}: !hello".format(message))
-        await message.channel.send('Hello!')
-
-    ## BASIC CRAIGSLIST SEARCH
-    if message.content.startswith("!cl_search") and message.channel.name == "craigslist":
-
-        arguments = message.content.lstrip('!cl_search').lstrip().rstrip()
-
-        '''
-        allKeys = ""
-        filterFree = False
-        filterTypes = ""
-        filterLocationData = ""
-        filterPriceData = ""
-        sort = "LowPrice"
-
-        try:
-            args = parser.parse_args([arguments])
-        except:
-            print ("FAILED PARSING")
-            args.keys = "synthesizer"
-            args.filterFree = False
-
-        for key in args.keys:
-            allKeys += key + "+"
-
-        if args.sort:
-            sort = args.sort
-
-        if args.filterLocationData:
-            filterTypes += "Locations , "
-            filterLocationData = args.filterLocationData
-
-        if args.filterPriceData:
-            filterTypes += "Prices , "
-            filterPriceData = args.filterPriceData
-
-        filterFree = args.filterFree
-        '''
-
-        Log("{0.author.mention}: !cl_search {1}".format(message, arguments))
-
-        header='**CraigsList Search**\nKeywords: {0}\nFilters: {1}\nSort: {2}'.format(arguments, "LowPrice", "NoFree")
-
-        msg = cl_search(arguments, sort="LowPrice", filterFree=True)
-
-        #cl_search(allKeys, filterTypes=filterTypes, filterLocationData=filterLocationData, filterPriceData=filterPriceData, sort=sort, filterFree=filterFree)
-        await message.channel.send(header + '```' + str(msg[:10]) + '```')
-
-    ### SIMPLE SEARCH
-    if message.content.startswith("!search") and message.channel.name == "craigslist":
-
-        arguments = message.content.lstrip('!search').lstrip().rstrip()
-
-        Log("{0.author.mention}: !cl_search {1}".format(message, arguments))
-
-        header='**CraigsList Search**\nKeywords: {0}\nFilters: {1}\nSort: {2}'.format(arguments, "LowPrice", "NoFree")
-
-        msg = cl_search(arguments.replace(" ", "+"), sort="LowPrice", filterFree=True)
-
-        await message.channel.send(header + '```' + str(msg[:10]) + '```')
-
-@client.event
+@bot.event
 async def on_ready():
     print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print(bot.user.name)
+    print(bot.user.id)
     print('------')
 
-client.run(token)
+@bot.command(pass_context=True)
+async def ping(ctx):
+    '''
+    Returns the bot's latency
+    '''
+    latency = bot.latency
+    await ctx.send(botMsg(str(round(latency * 1000, 3)) + "ms"))
+
+@bot.command(name='echo', pass_context=True)
+async def echo(ctx, *, content:str):
+    '''
+    Reply with the user's parameters
+    '''
+    await ctx.send(botMsg(content))
+
+@bot.command(name="search", pass_context=True)
+async def search(ctx, *, args:str):
+    '''
+    Search CraigsList and Format Results
+    '''
+
+    arguments = "-s Alpha -l VANCOUVER -p 200-300 -k synth dx7"
+
+    arg = arguments.split(" ")
+
+    for x in range(len(arguments))
+
+    #Decipher args for options
+    args = "DX7"
+
+    header='**CraigsList Search**\nKeywords: {0}\nFilters: {1}\nSort: {2}'.format(args, "LowPrice", "NoFree")
+    msg = cl_search(args.replace(" ", "+"), sort="LowPrice", filterFree=True)
+    await ctx.send(botMsg(msg[:10]))
+
+bot.run(token)
--- test.py
+++ test.py
@@ -1,2 +1,65 @@
-import discord
-print ("Hello")
+arguments = "synth dx7 -f -s Alpha -l VANCOUVER -p 200-300"
+
+arg = arguments.split(" ")
+
+print(arg)
+
+sortType = "LowPrice"
+filterLocation = ""
+filterPrice = ""
+filterFree = False
+
+while (("-s" in arg) or ("-l" in arg) or ("-f" in arg) or ("-p" in arg)):
+    for x in range(len(arg)):
+        #print ("X: " + str(x))
+        checked = False
+        ## SORT
+        try:
+            if (('-s' in arg[x]) and (not checked)):
+                #print(arg[x+1])
+                sortType = arg.pop(x+1)
+                arg.pop(x)
+                checked = True
+        except:
+            pass
+
+        ## FILTER LOCATION
+        try:
+            if (('-l' in arg[x]) and (not checked)):
+                #print (arg[x+1])
+                filterLocation = arg.pop(x+1)
+                arg.pop(x)
+                checked = True
+        except:
+            pass
+
+        ## FILTER PRICE
+        try:
+            if (('-p' in arg[x]) and (not checked)):
+                #print (arg[x+1])
+                filterPrice = arg.pop(x+1)
+                arg.pop(x)
+                checked = True
+        except:
+            pass
+
+        ## FILTER FREE
+        try:
+            if (('-f' in arg[x]) and (not checked)):
+                #print(arg[x])
+                filterFree = True
+                arg.pop(x)
+                checked = True
+        except:
+            pass
+
+
+    ## Everything should be popped and only keywords left over
+print ("-------------------------------------")
+print(arg)
+print ("-------------------------------------")
+print ("Sort: " + sortType)
+print ("FilterLocation: " + filterLocation)
+print ("FilterPrice: " + filterPrice)
+print ("FilterFree: " + str(filterFree))
+print ("Keywords: " + str(arg))
